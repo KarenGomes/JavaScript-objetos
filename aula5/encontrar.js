bc9e2b864bac828dd1a1e732960ec019556f12f9
KarenGomes <karenecris12015@gmail.com>
2023-08-16 10:09:09 -0300

    Aula 5

---
+++ aula5/encontrar.js
@@ -0,0 +1,13 @@
+const clientes = require("./clientes.json");
+//console.log(clientes);
+
+function encontrar(lista, chave, valor){
+    return lista.find((item)=> item[chave].includes(valor));
+}
+
+const encontrado = encontrar(clientes, "nome", "Kirby");
+const encontrado2 = encontrar(clientes, "telefone", "1918820860"); 
+
+console.log(encontrado);
+console.log(encontrado2);
+
---
+++ aula5/ordenar.js
@@ -0,0 +1,17 @@
+const clientes = require("./clientes.json");
+
+function ordenar(lista, propriedade){
+    const resultado = lista.sort((a,b) => { //tem que especificar para o sort()
+        if(a[propriedade] < b[propriedade]){
+            return -1;
+        }
+        if(a[propriedade] > b[propriedade]){
+            return 1;
+        }
+        return 0;
+    })
+    return resultado;
+}
+
+const ordenadoNome = ordenar(clientes, "nome");
+console.log(ordenadoNome);
